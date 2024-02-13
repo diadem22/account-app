743302b95b82dba775ed9ea56cdacaad6e492060
Ifeoluwa Adeyemi <aifeoluwadiadem@gmail.com>
2024-02-13 17:38:06 +0100

    update guard

--- src/guard/user-view.guard.ts
+++ src/guard/user-view.guard.ts
@@ -22,12 +22,15 @@ export class UserViewGuard implements CanActivate {
         where: { uuid: decodedToken.uid },
       });
       const account = await this.sequelize.models.Account.findOne({
-        attributes: ['id'],
+        attributes: ['id', 'user_id'],
         where: { id: req.params.account_id },
       });
       if (user.dataValues.userType === 'B') {
         return true;
-      } else if (account.dataValues.id == req.params.account_id) {
+      } else if (
+        account.dataValues.id == req.params.account_id &&
+        account.dataValues.user_id == user.dataValues.id
+      ) {
         return true;
       }
       throw new UnauthorizedException('User not authorized');
